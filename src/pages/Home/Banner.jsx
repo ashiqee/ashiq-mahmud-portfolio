c24c1a24538ed1d824b3eb640ba8c7f14804e70c
ashiq73 <ashiq.buet73@gmail.com>
2023-12-11 10:41:45 +0600

    banner design

--- src/pages/Home/Banner.jsx
+++ src/pages/Home/Banner.jsx
@@ -1,13 +1,37 @@
-import profileImg from '../../assets/images/ashiq.png'
+import profileImg from "../../assets/images/ashiq.png";
 
 const Banner = () => {
-    return (
-        <div className="h-screen">
-            <h2>Ashik Mahmud</h2>
-            <img className='rounded-full' src={profileImg} alt="" />
+  return (
+    <div className="h-screen  md:flex flex md:flex-row flex-col-reverse items-center justify-around mx-auto container">
+      <div className="absolute text-9xl font-extrabold opacity-10 w-full bottom-0">
+        <div className="relative flex overflow-x-hidden">
+          <div className="py-12 animate-marquee whitespace-nowrap">
+            <span className=" mx-96"> ASHEK MAHMUD ASHIK</span>
+          </div>
 
+          <div className="absolute  py-12 animate-marquee2 whitespace-nowrap">
+            <span className=" mx-96">ASHEK MAHMUD ASHIK</span>
+          </div>
         </div>
-    );
+      </div>
+
+      <div className="space-y-6">
+        <h2 className="text-2xl  uppercase font-bold">
+          Ashek Mahmud <span className="text-[#00FFFF]">Ashik</span>
+        </h2>
+        <h2 className="text-6xl font-extrabold">
+          <span className="text-[#00FFFF]">MERN</span> STACK <br /> DEVELOPER
+        </h2>
+        <div className="flex gap-5 mt-10">
+          <button className="btn text-white btn-outline">GET RESUME</button>
+          <button className="btn text-[#00FFFF] btn-outline">CONTACT ME</button>
+        </div>
+      </div>
+      <div>
+        <img className="w-96" src={profileImg} alt="" />
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,119 +5,152 @@ import AboutUs from "../AboutUs/AboutUs";
 import Banner from "./Banner";
 import { useState } from "react";
 import Portfolio from "../Portfolio/Portfolio";
-import './home.scss'
-
+import "./home.scss";
 
 const Home = () => {
-    const [activePage, setActivePage] = useState('home')
+  const [activePage, setActivePage] = useState("home");
 
-    const handlePageChange = (page) => {
-        setActivePage(page)
+  const handlePageChange = (page) => {
+    setActivePage(page);
+  };
+
+  const handleScroll = (e) => {
+    if (e.deltaY > 0 && activePage !== "about") {
+      handlePageChange("about");
+    } else if (e.deltaY > 0 && activePage !== "skills") {
+      handlePageChange("skills");
+    } else if (e.deltaY > 0 && activePage !== "projects") {
+      handlePageChange("projects");
+    } else if (e.deltaY < 0 && activePage !== "home") {
+      handlePageChange("home");
     }
-
-
-    const handleScroll = (e) => {
-        if (e.deltaY > 0 && activePage !== 'about') {
-
-            handlePageChange("about")
-        }
-
-        else if (e.deltaY > 0 && activePage !== 'skills') {
-            handlePageChange('skills')
-        }
-        else if (e.deltaY > 0 && activePage !== 'projects') {
-            handlePageChange('projects')
-        }
-
-
-
-        else if (e.deltaY < 0 && activePage !== 'home') {
-            handlePageChange("home")
-        }
-
-    }
-    return (
-        <div onWheel={handleScroll} className="bg-gradient-to-r  text-white from-[#0c0c1d] to-slate-700">
-
-            {/* sidebar  */}
-            <div className="fixed right-0  ">
-                <div className=" ">
-                    <div className="grid 
+  };
+  return (
+    <div
+      onWheel={handleScroll}
+      className="bg-gradient-to-r  text-white from-[#0c0c1d] to-slate-700"
+    >
+      {/* sidebar  */}
+      <div className="fixed right-0  ">
+        <div className=" ">
+          <div
+            className="grid 
                  items-center 
                  justify-center w-1/2 my-48  text-white   
-                 gap-2">
+                 gap-2"
+          >
+            <Link
+              onClick={() => handlePageChange("home")}
+              className={
+                activePage === "home"
+                  ? "border text-[#00FFFF] rotate-90 my-12 rounded-md"
+                  : " border border-red-200 pr-4  hover:text-[#00FFFF]  rounded-md"
+              }
+            >
+              <div className="flex items-center gap-2">
+                <span
+                  className={
+                    activePage === "home"
+                      ? "bg-opacity-50 w-12 p-4 bg-[#00FFFF] -rotate-90  text-black text-xl"
+                      : "bg-gray-600 bg-opacity-50 w-12 p-4  hover:bg-[#00FFFF]  hover:text-black text-xl"
+                  }
+                >
+                  <FaHome />
+                </span>
+                <h3 className="hover:scale-x-110 mx-auto transition-transform transform">
+                  HOME
+                </h3>
+              </div>
+            </Link>
+            <Link
+              onClick={() => handlePageChange("about")}
+              className={
+                activePage === "about"
+                  ? "border text-[#00FFFF] rotate-90 my-12 rounded-md"
+                  : " border border-red-200 pr-4  hover:text-[#00FFFF]  rounded-md"
+              }
+            >
+              <div className="flex items-center gap-2">
+                <span
+                  className={
+                    activePage === "about"
+                      ? "bg-opacity-50 w-12 p-4 bg-[#00FFFF] -rotate-90 text-black text-xl"
+                      : "bg-gray-600 bg-opacity-50 w-12 p-4  hover:bg-[#00FFFF]  hover:text-black text-xl"
+                  }
+                >
+                  <FcAbout />
+                </span>
+                <h3 className="hover:scale-x-110 mx-auto transition-transform transform">
+                  ABOUT
+                </h3>
+              </div>
+            </Link>
+            <Link
+              onClick={() => handlePageChange("skills")}
+              className={
+                activePage === "skills"
+                  ? "border text-[#00FFFF] rotate-90 my-12 rounded-md"
+                  : " border border-red-200 pr-4  hover:text-[#00FFFF]  rounded-md"
+              }
+            >
+              <div className="flex items-center gap-2">
+                <span
+                  className={
+                    activePage === "skills"
+                      ? "bg-opacity-50 w-12 p-4 bg-[#00FFFF] -rotate-90  text-black text-xl"
+                      : "bg-gray-600 bg-opacity-50 w-12 p-4  hover:bg-[#00FFFF]  hover:text-black text-xl"
+                  }
+                >
+                  <FaSkiing />
+                </span>
+                <h3 className="hover:scale-x-110 mx-auto  transition-transform transform">
+                  SKILLS
+                </h3>
+              </div>
+            </Link>
+            <Link
+              onClick={() => handlePageChange("projects")}
+              className={
+                activePage === "projects"
+                  ? "border pr-4  text-[#00FFFF] rotate-90 my-12 rounded-md"
+                  : " border border-red-200 pr-4  hover:text-[#00FFFF]  rounded-md"
+              }
+            >
+              <div className="flex items-center gap-2">
+                <span
+                  className={
+                    activePage === "projects"
+                      ? "bg-opacity-50 w-12 p-4 bg-[#00FFFF] -rotate-90 text-black text-xl"
+                      : "bg-gray-600 bg-opacity-50 w-12 p-4  hover:bg-[#00FFFF]  hover:text-black text-xl"
+                  }
+                >
+                  <FaSkiing />
+                </span>
+                <h3 className="hover:scale-x-110 mx-auto  transition-transform transform">
+                  PROJECTS
+                </h3>
+              </div>
+            </Link>
+          </div>
+        </div>
+      </div>
 
-                        <Link onClick={() => handlePageChange('home')}
-                            className={activePage === 'home' ? "border text-[#00FFFF] rounded-md" : " border border-red-200 pr-4  hover:text-[#00FFFF]  rounded-md"} >
-                            <div className="flex items-center gap-2">
-                                <span className={activePage === 'home' ? "bg-opacity-50 w-12 p-4 bg-[#00FFFF]  text-black text-xl" : "bg-gray-600 bg-opacity-50 w-12 p-4  hover:bg-[#00FFFF]  hover:text-black text-xl"}><FaHome /></span>
-                                <h3 className="hover:scale-x-110  transition-transform transform">
-                                    HOME
-                                </h3>
-                            </div>
-                        </Link>
-                        <Link onClick={() => handlePageChange('about')}
-                            className={activePage === 'about' ? "border text-[#00FFFF] rounded-md" : " border border-red-200 pr-4  hover:text-[#00FFFF]  rounded-md"} >
-                            <div className="flex items-center gap-2">
-                                <span className={activePage === 'about' ? "bg-opacity-50 w-12 p-4 bg-[#00FFFF]  text-black text-xl"
-                                    : "bg-gray-600 bg-opacity-50 w-12 p-4  hover:bg-[#00FFFF]  hover:text-black text-xl"}>
-                                    <FcAbout /></span>
-                                <h3 className="hover:scale-x-110  transition-transform transform">
-                                    ABOUT
-                                </h3>
-                            </div>
-                        </Link>
-                        <Link onClick={() => handlePageChange('skills')}
-                            className={activePage === 'skills' ? "border text-[#00FFFF] rounded-md" : " border border-red-200 pr-4  hover:text-[#00FFFF]  rounded-md"} >
-                            <div className="flex items-center gap-2">
-                                <span className={activePage === 'skills' ? "bg-opacity-50 w-12 p-4 bg-[#00FFFF]  text-black text-xl"
-                                    : "bg-gray-600 bg-opacity-50 w-12 p-4  hover:bg-[#00FFFF]  hover:text-black text-xl"}>
-                                    <FaSkiing /></span>
-                                <h3 className="hover:scale-x-110  transition-transform transform">
-                                    SKILLS
-                                </h3>
-                            </div>
-                        </Link>
-                        <Link onClick={() => handlePageChange('projects')}
-                            className={activePage === 'projects' ? "border pr-4  text-[#00FFFF] rounded-md" : " border border-red-200 pr-4  hover:text-[#00FFFF]  rounded-md"} >
-                            <div className="flex items-center gap-2">
-                                <span className={activePage === 'projects' ? "bg-opacity-50 w-12 p-4 bg-[#00FFFF]  text-black text-xl"
-                                    : "bg-gray-600 bg-opacity-50 w-12 p-4  hover:bg-[#00FFFF]  hover:text-black text-xl"}>
-                                    <FaSkiing /></span>
-                                <h3 className="hover:scale-x-110  transition-transform transform">
-                                    PROJECTS
-                                </h3>
-                            </div>
-                        </Link>
+      {/* main Home Page  */}
+      <div className="overflow-y-auto">
+        <section>{activePage === "home" && <Banner />}</section>
+        <section>{activePage === "about" && <AboutUs />}</section>
 
+        <section>{activePage === "skills" && <Portfolio />}</section>
+      </div>
 
-
-                    </div>
-                </div>
-            </div >
-
-            {/* main Home Page  */}
-            < div className="overflow-y-auto" >
-
-                <section>{activePage === 'home' && <Banner />}</section>
-                <section>
-
-                    {activePage === 'about' && <AboutUs />}
-                </section>
-
-                <section>
-
-                    {activePage === 'skills' && <Portfolio />}
-                </section>
-
-            </div >
-
-            <button onClick={() => handlePageChange("home")} className="absolute text-3xl bottom-2 right-0 p-4">
-                <FaArrowAltCircleUp />
-            </button>
-
-        </div >
-    );
+      <button
+        onClick={() => handlePageChange("home")}
+        className="absolute text-3xl bottom-2 right-0 p-4"
+      >
+        <FaArrowAltCircleUp />
+      </button>
+    </div>
+  );
 };
 
 export default Home;
